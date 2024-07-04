cf5e50d7de802792c6b6dbef724b4b2b44b00fc5
Joás Feitosa <joasfeitosadelimasilva@gmail.com>
2024-07-04 13:29:02 -0300

    first commit

---
+++ JornadaMilhas.Test.Integracao/OfertaViagemDalAdicionar.cs
@@ -0,0 +1,33 @@
+using JornadaMilhas.Dados;
+using JornadaMilhasV1.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace JornadaMilhas.Test.Integracao;
+
+public class OfertaViagemDalAdicionar
+{
+    [Fact]
+    public void RegistraOfertaNoBanco()
+    {
+        //arrange
+        Rota rota = new Rota("São Paulo", "Fortaleza");
+        Periodo periodo = new Periodo(new DateTime(2024, 8, 20), new DateTime(2024, 8, 30));
+        double preco = 350;
+
+        var options = new DbContextOptionsBuilder<JornadaMilhasContext>()
+            .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=JornadaMilhas;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False")
+            .Options;
+
+        var context = new JornadaMilhasContext(options);
+
+        var oferta = new OfertaViagem(rota, periodo, preco);
+        var dal = new OfertaViagemDAL(context);
+        //act
+        dal.Adicionar(oferta);
+
+        //assert
+        var ofertaIncluida = dal.RecuperarPorId(oferta.Id);
+        Assert.NotNull(ofertaIncluida);
+        Assert.Equal(ofertaIncluida.Preco, oferta.Preco, 0.001);
+    }
+}
